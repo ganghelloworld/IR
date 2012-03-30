97b89a3f5f932342e5b9ee8965ac37e94ccd5476
gang <gang@gang-PC.(none)>
2012-03-30 16:22:48 +0800

    tree shit

--- greed_algorithms/huffman_code.cpp
+++ greed_algorithms/huffman_code.cpp
@@ -1,4 +1,5 @@
 #include "huffman_code.h"
+#include "../include/constant.h"
 #include <iostream>
 #include <string>
 
@@ -7,16 +8,45 @@ using namespace std;
 HuffmanCode::HuffmanCode(CharUnit* c, int n):
 	cu(0), num(n)
 {
-	cu = new CharUnit[num];
+	tree = new TreeNode<CharUnit>[num];
 	for(int i = 0; i < num; i++)
 	{
-		cu[i] = c[i];
+		tree[i] = TreeNode<CharUnit>(c[i]);
 	}
+	size = num;
+	coding();
 }
 void HuffmanCode::coding()
 {
-	bool tag = new bool[num];
-	for(int = 0; i < num; i++) tag[i] = false;
+	while(size > 1)
+	{
+		TreeNode<CharUnit>* root = new TreeNode<CharUnit*>;
+		TreeNode<CharUnit> temp;
+
+		int left = find_min_index();
+		size--;
+		temp = tree[left];
+		tree[left] = tree[size];
+		tree[size] = temp_node;
+		int right = find_min_index();
+
+		temp_node = tree[left];
+		root.set_child(&temp_node, &tree[size]);
+	}
+}
+int HuffmanCode::find_min_index()
+{
+	int temp = tree[0].data.fre;
+	int index = 0;
+	for(int i = 1; i < size; i++)
+	{
+		if(tree[i].data.fre < temp)
+		{
+			temp = tree[i].data.fre;	
+			index = i;
+		}
+	}
+	return index;
 }
 void HuffmanCode::output_code() const
 {
--- greed_algorithms/huffman_code.h
+++ greed_algorithms/huffman_code.h
@@ -2,7 +2,30 @@
 #define HUFFMAN_CODE_H
 
 #include <string>
-
+template <class T>
+class TreeNode
+{
+	TreeNode* left;
+	TreeNode* right;
+	T data;
+public:
+	TreeNode() : left(0), right(0){};
+	TreeNode(T t) : left(0), right(0), data(t){};
+	TreeNode& operator=(const TreeNode& tn)
+	{
+		left = tn.left;
+		right = tn.right;
+		data = tn.data;
+	}
+	void set_child(TreeNode* l, TreeNode* r)
+	{
+		left = l;
+		right = r;
+		data.fre = l.data.fre + r.data.fre;
+	}
+	friend class HuffmanCode;
+	friend class CharUnit;
+};
 class CharUnit
 {
 	char ch;
@@ -30,10 +53,12 @@ public:
 
 class HuffmanCode
 {
-	CharUnit* cu;
+	TreeNode<CharUnit>* tree;
 	int num;
+	int size;
 
 	void coding();
+	int find_min_index();
 public:
 	HuffmanCode(){};
 	HuffmanCode(CharUnit*, int);	
